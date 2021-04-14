61048ea8d090815305721d408e3a41f07e0049ad
Dheeraj-Sachan <dheerajsachan009@gmail.com>
2021-04-14 08:09:40 +0530

    third commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		///hello
-		System.out.println("zhello");
+		System.out.println("buffalo");
 	}
 
 }
